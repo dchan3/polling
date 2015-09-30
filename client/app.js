579cc3d40418addefd468e96de4772ad3486a72a
dchan3 <dchan3@hawaii.edu>
2015-09-30 09:35:49 -1000

    IC 11

---
+++ client/app.js
@@ -0,0 +1,19 @@
+/**
+ * Created by sunquan8094 on 9/30/15.
+ */
+Template.body.helpers({
+
+    polls: function() {
+        return Polls.find();
+    }
+
+});
+
+UI.registerHelper('indexedArray', function(context, options) {
+    if (context) {
+        return context.map(function(item, index) {
+            item._index = index;
+            return item;
+        });
+    }
+});
